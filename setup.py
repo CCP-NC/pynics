2bbab9b02422afabfa696fe5ca742e2f18f0c839
stur86 <simonesturniolo@gmail.com>
2018-11-02 16:58:10 +0000

    Added dependencies explicitly

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(name='pynics',
       version='0.1.0',
       packages=['pynics'],
+      install_requires=[
+          'numpy',
+          'ase'
+      ],      
       entry_points={
           'console_scripts': [
               'lorentz_buildup_nics = pynics.__main__:nics_buildup'
