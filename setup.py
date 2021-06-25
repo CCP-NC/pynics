a6d0c726c726138a16a140b11b582d5be54c1172
Ben Tatman <ben@tatmans.co.uk>
2021-06-25 15:44:29 +0100

    Find_packages to include binparse and freeform subpackages

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='pynics',
       version='0.1.0',
-      packages=['pynics'],
+      packages=find_packages(),
       install_requires=[
           'numpy',
           'ase'
@@ -12,4 +12,5 @@ setup(name='pynics',
               'lorentz_buildup_nics = pynics.__main__:nics_buildup'
           ]
       },
-      )
+      include_package_data=True,
+)
